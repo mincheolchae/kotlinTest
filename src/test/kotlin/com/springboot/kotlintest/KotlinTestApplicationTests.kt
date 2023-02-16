c31bdf78b6841c45fa10ac9270a452a69b87ab9e
mincheolchae <cmc752@gmail.com>
2023-02-16 15:15:13 +0900

    kotlin 프로젝트 생성

---
+++ src/test/kotlin/com/springboot/kotlintest/KotlinTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.springboot.kotlintest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
